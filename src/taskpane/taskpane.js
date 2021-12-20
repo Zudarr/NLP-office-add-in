c9f4ae52d0469234cd91901c409ac88ef98f764c
kurei01 <ku12322132@gmail.com>
2021-12-20 14:00:56 +0700

    kurei

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -6,25 +6,81 @@
 /* global document, Office, Word */
 
 Office.onReady((info) => {
+  // Determine if the user's version of Office supports all the Office.js APIs that are used in the tutorial.
+  if (!Office.context.requirements.isSetSupported("WordApi", "1.3")) {
+    console.log("Sorry. The tutorial add-in uses Word.js APIs that are not available in your version of Office.");
+  }
+
+  // Assign event handlers and other initialization logic.
+  document.getElementById("insert-paragraph").onclick = insertParagraph;
+  document.getElementById("apply-style").onclick = applyStyle;
+  document.getElementById("apply-custom-style").onclick = applyCustomStyle;
+  document.getElementById("change-font").onclick = changeFont;
   if (info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
   }
 });
 
-export async function run() {
-  return Word.run(async (context) => {
-    /**
-     * Insert your Word code here
-     */
+function insertParagraph() {
+  Word.run(function (context) {
+    var docBody = context.document.body;
+    docBody.insertParagraph(
+      "Office has several versions, including Office 2016, Microsoft 365 subscription, and Office on the web.",
+      "Start"
+    );
 
-    // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+    return context.sync();
+  }).catch(function (error) {
+    console.log("Error: " + error);
+    if (error instanceof OfficeExtension.Error) {
+      console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    }
+  });
+}
 
-    // change the paragraph color to blue.
-    paragraph.font.color = "blue";
+function applyStyle() {
+  Word.run(function (context) {
+    var firstParagraph = context.document.body.paragraphs.getFirst();
+    firstParagraph.styleBuiltIn = Word.Style.intenseReference;
 
-    await context.sync();
+    return context.sync();
+  }).catch(function (error) {
+    console.log("Error: " + error);
+    if (error instanceof OfficeExtension.Error) {
+      console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    }
+  });
+}
+
+function applyCustomStyle() {
+  Word.run(function (context) {
+    var lastParagraph = context.document.body.paragraphs.getLast();
+    lastParagraph.style = "MyCustomStyle";
+
+    return context.sync();
+  }).catch(function (error) {
+    console.log("Error: " + error);
+    if (error instanceof OfficeExtension.Error) {
+      console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    }
+  });
+}
+
+function changeFont() {
+  Word.run(function (context) {
+    var secondParagraph = context.document.body.paragraphs.getFirst().getNext();
+    secondParagraph.font.set({
+      name: "Courier New",
+      bold: true,
+      size: 18,
+    });
+
+    return context.sync();
+  }).catch(function (error) {
+    console.log("Error: " + error);
+    if (error instanceof OfficeExtension.Error) {
+      console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    }
   });
 }
