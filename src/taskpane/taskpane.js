0fb112c0e647dcba304cd6427b56267a1b1e7b33
kurei01 <ku12322132@gmail.com>
2022-01-03 20:08:19 +0700

    notthing

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-redeclare */
 /* eslint-disable no-undef */
 /*
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
@@ -14,6 +15,7 @@ Office.onReady((info) => {
 
   // Assign event handlers and other initialization logic.
   document.getElementById("test").onclick = test;
+  document.getElementById("read-selected-range").onclick = readSelectedRange;
   if (info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
@@ -28,6 +30,41 @@ function test() {
       .sync()
       .then(function () {
         paragraphs.InsertText("../src/PythonCode/output.txt", Word.InsertLocation.end);
+      })
+      .then(context.sync);
+  }).catch(function (error) {
+    console.log("Error: " + error);
+    if (error instanceof OfficeExtension.Error) {
+      console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    }
+  });
+}
+
+function readSelectedRange() {
+  Word.run(function (context) {
+    var doc = context.document;
+    // load entire text of document body
+    var wholeDoc = doc.body;
+    wholeDoc.load("text");
+    // load text of selected range
+    var selectedRange = doc.getSelection();
+    selectedRange.load("text");
+    return context
+      .sync()
+      .then(function () {
+        if (selectedRange.text != "") {
+          var text = selectedRange.text;
+        } else {
+          var text = wholeDoc.text;
+        }
+        var statisticResult = "";
+        var child_process = require("child_process").spawn;
+        var process = child_process("python", ["../PythonCode/NLP.py", text]);
+        process.stdout.on("data", (data) => {
+          statisticResult = data.toString();
+        });
+        doc.body.insertParagraph(statisticResult, "End");
+        //doc.body.insertParagraph(text, "End");
       })
       .then(context.sync);
   }).catch(function (error) {
