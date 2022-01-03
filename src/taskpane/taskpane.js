09718bb25c2c38d445c155b3e81603be4e1a15da
kurei01 <ku12322132@gmail.com>
2022-01-03 15:24:22 +0700

    format

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,9 +1,10 @@
-const RunPython = require('./run_python');
+/* eslint-disable no-undef */
+const RunPython = require("./run_python");
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
-    if (!Office.context.requirements.isSetSupported('WordApi', '1.3')) {
-      console.log('Sorry. The tutorial add-in uses Word.js APIs that are not available in your version of Office.');
+    if (!Office.context.requirements.isSetSupported("WordApi", "1.3")) {
+      console.log("Sorry. The tutorial add-in uses Word.js APIs that are not available in your version of Office.");
     }
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
@@ -16,25 +17,26 @@ function readSelectedRange() {
     var doc = context.document;
     // load entire text of document body
     var wholeDoc = doc.body;
-    wholeDoc.load('text');
+    wholeDoc.load("text");
     // load text of selected range
     var selectedRange = doc.getSelection();
     selectedRange.load("text");
-    return context.sync()
-      .then(function() {
-        if(selectedRange.text != ''){
+    return context
+      .sync()
+      .then(function () {
+        if (selectedRange.text != "") {
           var text = RunPython.runPython(selectedRange.text);
-        }else{
+        } else {
+          // eslint-disable-next-line no-redeclare
           var text = RunPython.runPython(wholeDoc.text);
         }
-        doc.body.insertParagraph(text,'End');
+        doc.body.insertParagraph(text, "End");
       })
       .then(context.sync);
-  })
-  .catch(function (error) {
-      console.log("Error: " + error);
-      if (error instanceof OfficeExtension.Error) {
-          console.log("Debug info: " + JSON.stringify(error.debugInfo));
-      }
+  }).catch(function (error) {
+    console.log("Error: " + error);
+    if (error instanceof OfficeExtension.Error) {
+      console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    }
   });
 }
